9d4a79447f3ccfd59f1997104699c9fb503699bf
Rosspal <gukov1999@yandex.ru>
2022-01-17 00:43:23 +0900

    start

---
+++ Heart of Darkness/Assets/Scripts/GameControler.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControler : MonoBehaviour
+{
+    private EnemyTeam EnemyTeam;
+    private HeroTeam Team;
+}
---
+++ Heart of Darkness/Assets/Scripts/Person.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Person : MonoBehaviour
+{
+    public Health Health;
+    public Characteristic Characteristic;
+
+    public int peremPerson = 15;
+
+}
