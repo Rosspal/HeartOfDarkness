57fbb88c86424f831b2415d539a2c274a95c22d1
Rosspal <gukov1999@yandex.ru>
2022-02-25 21:05:38 +0900

    Ui update

--- HeartOfDarkness/Assets/Scenes/Map/mapLevel1/road and river1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="road and river1" tilewidth="32" tileheight="48" tilecount="220" columns="22">
- <image source="road and river/0NyCh9.png" width="704" height="480"/>
-</tileset>
