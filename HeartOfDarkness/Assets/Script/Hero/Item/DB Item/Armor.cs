b88b167c8af555ebc987406ccbe76dab040f320c
Rosspal <gukov1999@yandex.ru>
2022-03-14 09:13:28 +0900

    mini update

---
+++ HeartOfDarkness/Assets/Script/Hero/Item/DB Item/Armor.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ArmorDate", menuName = "Item/armor")]
+public class Armor : Item
+{
+    public string armorType;
+    public int armorClass;
+}
--- HeartOfDarkness/Assets/Script/Hero/Item/Equipment.cs
+++ HeartOfDarkness/Assets/Script/Hero/Item/Equipment.cs
@@ -4,117 +4,117 @@ using UnityEngine;
 
 public class Equipment : MonoBehaviour
 {
-    private Weapon hand;
-    private Armor helmet;
-    private Armor footwear;
-    private Armor pants;
-    private Armor torso;
-    private Belt belt;
-    private Accessory oneRing;
-    private Accessory twoRing;
-    private Accessory gloves;
-    private Accessory amulet;
+    //private Weapon hand;
+    //private Armor helmet;
+    //private Armor footwear;
+    //private Armor pants;
+    //private Armor torso;
+    //private Belt belt;
+    //private Item oneRing;
+    //private Item twoRing;
+    //private Item gloves;
+    //private Item amulet;
 
-    public Equipment() { }
+    //public Equipment() { }
 
-    public Weapon Hand { get => hand; set => hand = value; }
-    public Armor Helmet { get => helmet; set => helmet = value; }
-    public Armor Footwear { get => footwear; set => footwear = value; }
-    public Armor Pants { get => pants; set => pants = value; }
-    public Armor Torso { get => torso; set => torso = value; }
-    public Belt Belt { get => belt; set => belt = value; }
-    public Accessory OneRing { get => oneRing; set => oneRing = value; }
-    public Accessory TwoRing { get => twoRing; set => twoRing = value; }
-    public Accessory Gloves { get => gloves; set => gloves = value; }
-    public Accessory Amulet { get => amulet; set => amulet = value; }
+    //public Weapon Hand { get => hand; set => hand = value; }
+    //public Armor Helmet { get => helmet; set => helmet = value; }
+    //public Armor Footwear { get => footwear; set => footwear = value; }
+    //public Armor Pants { get => pants; set => pants = value; }
+    //public Armor Torso { get => torso; set => torso = value; }
+    //public Belt Belt { get => belt; set => belt = value; }
+    //public Item OneRing { get => oneRing; set => oneRing = value; }
+    //public Item TwoRing { get => twoRing; set => twoRing = value; }
+    //public Item Gloves { get => gloves; set => gloves = value; }
+    //public Item Amulet { get => amulet; set => amulet = value; }
 
-    public void SetItem(Accessory item)
-    {
-        switch (item.Type)
-        {
-            case "oneRing":
-                oneRing = item;
-                break;
-            case "twoRing":
-                twoRing = item;
-                break;
-            case "gloves":
-                gloves = item;
-                break;
-            case "amulet":
-                amulet = item;
-                break;
-        }
-    }
+    //public void SetItem(Item item)
+    //{
+    //    switch (item.type)
+    //    {
+    //        case "oneRing":
+    //            oneRing = item;
+    //            break;
+    //        case "twoRing":
+    //            twoRing = item;
+    //            break;
+    //        case "gloves":
+    //            gloves = item;
+    //            break;
+    //        case "amulet":
+    //            amulet = item;
+    //            break;
+    //    }
+    //}
 
-    public void SetItem(Weapon item)
-    {
-        hand = item;
-    }
+    //public void SetItem(Weapon item)
+    //{
+    //    hand = item;
+    //}
 
-    public void SetItem(Armor item)
-    {
-        switch(item.Type){
-            case "helmet":
-                helmet = item;
-                break;
-            case "footwear":
-                footwear = item;
-                break;
-            case "pants":
-                pants = item;
-                break;
-            case "torso":
-                torso = item;
-                break;
-        }
-    }
+    //public void SetItem(Armor item)
+    //{
+    //    switch(item.type){
+    //        case "helmet":
+    //            helmet = item;
+    //            break;
+    //        case "footwear":
+    //            footwear = item;
+    //            break;
+    //        case "pants":
+    //            pants = item;
+    //            break;
+    //        case "torso":
+    //            torso = item;
+    //            break;
+    //    }
+    //}
 
-    public void SetItem(Belt item)
-    {
-        belt = item;
-    }
+    //public void SetItem(Belt item)
+    //{
+    //    belt = item;
+    //}
 
-    public void TakeOff(string type)
-    {
-        switch (type)
-        {
-            case "helmet":
-                helmet = new Armor();
-                break;
-            case "footwear":
-                footwear = new Armor();
-                break;
-            case "pants":
-                pants = new Armor();
-                break;
-            case "torso":
-                torso = new Armor();
-                break;
-            case "oneRing":
-                oneRing = new Accessory();
-                break;
-            case "twoRing":
-                twoRing = new Accessory();
-                break;
-            case "gloves":
-                gloves = new Accessory();
-                break;
-            case "amulet":
-                amulet = new Accessory();
-                break;
-            case "weapon":
-                hand = new Weapon();
-                break;
-            case "belt":
-                belt = new Belt();
-                break;
-        }
-    }
+    //public void TakeOff(string type)
+    //{
+    //    switch (type)
+    //    {
+    //        case "helmet":
+    //            helmet = new Armor();
+    //            break;
+    //        case "footwear":
+    //            footwear = new Armor();
+    //            break;
+    //        case "pants":
+    //            pants = new Armor();
+    //            break;
+    //        case "torso":
+    //            torso = new Armor();
+    //            break;
+    //        case "oneRing":
+    //            oneRing = new Item();
+    //            break;
+    //        case "twoRing":
+    //            twoRing = new Item();
+    //            break;
+    //        case "gloves":
+    //            gloves = new Item();
+    //            break;
+    //        case "amulet":
+    //            amulet = new Item();
+    //            break;
+    //        case "weapon":
+    //            hand = new Weapon();
+    //            break;
+    //        case "belt":
+    //            belt = new Belt();
+    //            break;
+    //    }
+    //}
 
-    //вывод общей брони
-    public int GetAllArmor()
-    {
-        return helmet.ArmorClass + footwear.ArmorClass + pants.ArmorClass + torso.ArmorClass;
-    }
+    ////вывод общей брони
+    //public int GetAllArmor()
+    //{
+    //    return helmet.armorClass + footwear.armorClass + pants.armorClass + torso.armorClass;
+    //}
 }
